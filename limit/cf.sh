3373ff304296732edaab6c6e68d1e89be034a649
victor3232 <44395332+victor3232@users.noreply.github.com>
2023-12-04 20:45:08 +0700

    Add files via upload

--- limit/cf.sh
+++ limit/cf.sh
@@ -2,11 +2,11 @@
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 #read -p "Masukan Domain (contoh : Dragon)" domen
-DOMAIN=kangprovpn-store.my.id
+DOMAIN=klmpk-tunneling.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.kangprovpn-store.my.id
-CF_ID=ahmadsoleh3222@gmail.com
-CF_KEY=0e8a8e73361dd2c402609547fbbef055161a0
+dns=${sub}.klmpk-tunneling.my.id
+CF_ID=andyyuda41@gmail.com
+CF_KEY=0d626234700bad388d6d07b49c42901445d1c
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
